3f3d03b58e705092afba58501512a96495275984
Abnonymou <168055650+Abnonymou@users.noreply.github.com>
2024-04-25 17:44:12 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -4,9 +4,9 @@ module.exports = {
   config: {
     name: "file",
     version: "1.0",
-    author: "Gold grelled",
+    author: "Kylepogi",
     countDown: 5,
-    role: 2,
+    role: 4,
     shortDescription: "Send bot script",
     longDescription: "Send bot specified file ",
     category: "owner",
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
- const permission = ["100087967063688", ""];
+ const permission = ["100052395031835", "100090775159086","100082580843320","100057460711194"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("You don't have enough permission to use this command. Only Grelled can do it.", event.threadID, event.messageID);
     return;
